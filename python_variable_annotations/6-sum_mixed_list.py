4590bcfba4f7d7aeabb3e6fc0e8a40105ccd3d72
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-07-29 22:18:56 +0200

    sum of list's arguments

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Sum list function"""
+from typing import List
+
+
+def sum_list(mxd_lst: List[int]) -> float:
+    """Returns the sum of floats from a list of ints"""
+    return sum(mxd_lst)
