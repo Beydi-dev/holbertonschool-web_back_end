079add7daeb8692d940ab0d0fc648d7a24d232e1
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-07-30 14:06:59 +0200

    random delay

---
+++ python_async_function/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import asyncio
+
+async def say_hello():
+    await asyncio.sleep(1)
+    print("Hello")
+
+async def say_world():
+    await asyncio.sleep(1)
+    print("World")
+
+async def main():
+    await say_hello()
+    await say_world()
+
+asyncio.run(main())
