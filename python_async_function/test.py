91e0434ca868b5e933680eccedc751167e0b7109
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-07-31 01:11:14 +0200

    ask 1

--- python_async_function/test.py
+++ python_async_function/test.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 import asyncio
 
-async def say_hello():
-    await asyncio.sleep(1)
-    print("Hello")
+async def attendre():
+    print("Je commence à attendre")
+    await asyncio.sleep(2)
+    print("J’ai fini d’attendre")
 
-async def say_world():
-    await asyncio.sleep(1)
-    print("World")
+async def autre_tache():
+    for i in range(4):
+        print(f"Autre tâche : {i}")
+        await asyncio.sleep(0.5)
 
 async def main():
-    await say_hello()
-    await say_world()
+    # On lance les deux coroutines en parallèle
+    await asyncio.gather(attendre(), autre_tache())
 
 asyncio.run(main())
