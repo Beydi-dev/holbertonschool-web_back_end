4d9eba2ee0dac000a21d60ace8296988de38d5ce
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-08-11 17:29:14 +0200

    simple helper

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" 0-simple_helper_function module """
-from typing import Tuple
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page, page_size):
     """pagination function that returns a tuple"""
+    if page < 1 or page_size < 1:
+        raise ValueError("page et page_size doivent être des entiers positifs")
     start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
