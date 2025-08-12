a3dceb250db93cc8a8c62c2c77cc71652f0fd9c0
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-08-12 13:36:51 +0200

    simple helper

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+"""
+Simple helper function for pagination
+"""
 
 def index_range(page: int, page_size: int) -> tuple:
-    """pagination function that returns a tuple"""
+    """
+    pagination function that returns a tuple
+    """
 
     start = (page - 1) * page_size
     end = start + page_size
