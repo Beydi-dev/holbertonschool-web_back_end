5061fe4263e64961fa634b87ef0ac9a540df4318
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-08-19 17:04:51 +0200

    9

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -2,6 +2,7 @@ import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
+
   return {
     ...budget,
     getIncomeInDollars(income) {
@@ -10,5 +11,5 @@ export default function getFullBudgetObject(income, gdp, capita) {
     getIncomeInEuros(income) {
       return `${income} euros`;
     },
-};
+  };
 }
