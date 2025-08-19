ac2e6435ae8e51bc66bf730d20434f64c2728ea0
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-08-19 16:57:29 +0200

    8

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -4,12 +4,9 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {
-
-	[`income-${getCurrentYear()}`]: income,
-	[`gdp-${getCurrentYear()}`]: gdp,
-	[`capita-${getCurrentYear()}`]: capita,
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
   };
-  
-  return budget;
 }
