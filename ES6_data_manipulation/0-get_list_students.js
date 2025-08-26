b6a8d34a7658bd77f8137c1a8eec669d5255a7ec
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-08-26 12:15:43 +0200

    task 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+function getListStudents() {
+	return [
+		{firstName: "Guillaume", id: 1, city: "San Francisco"},
+		{firstName: "James", id: 2, city: "Columbia"},
+		{firstName: "Serena", id: 5, city: "San Francisco"}
+	];
+}
