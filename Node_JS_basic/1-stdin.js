5f2aa083d6af329c6b724f49d81b6d943c9af2f4
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-09-02 17:38:21 +0200

    task1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,5 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
+  // On utiliser process.stdout plutôt que console log pour eviter \n
   process.stdout.write(`Your name is: ${data.toString()}`);
   console.log('This important software is now closing');
   process.exit();
